140c511fdb87f1735b3f947bd55d4077613e30f2
陈海新 <chenhaixin@szboanda.net>
2017-10-24 20:16:05 +0800

    添加单例模式

---
+++ src/main/java/com/noteshare/designPatterns/singleton/EagerlySingleton.java
@@ -0,0 +1,31 @@
+package com.noteshare.designPatterns.singleton;
+
+
+/**
+* @Title:  饿汉模式：提前new出来实例了，并不是在第一次调用get方法时才实例化，没有进行延迟加载。
+* 此处为了更好了的理解你需要去了解下类的饿汉和懒汉加载方式
+* @author  NoteShare
+* @since   JDK1.8
+* @history 2017年10月24日
+*/
+public class EagerlySingleton {
+    //类加载时进行对象实例化
+    private static EagerlySingleton eagerlySingleton = new EagerlySingleton();
+    /**
+     * 私有的构造方法，不让在外部进行类的实例化
+     */
+    private EagerlySingleton(){
+        
+    }
+    /**
+     * @Title      	: getInstance 
+     * @Description	: 开发对外的获取实例的方法
+     * @author     	: NoteShare
+     * Create Date 	: 2017年10月24日 下午4:09:19
+     * @return
+     * @throws
+     */
+    public static EagerlySingleton getInstance(){
+        return eagerlySingleton;
+    }
+}
---
+++ src/main/java/com/noteshare/designPatterns/singleton/LazySingleton.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+ * Copyright (C) ShenZhen Powerdata Information Technology Co.,Ltd All Rights Reserved.
+ * 本软件为深圳市博安达信息技术股份有限公司开发研制。未经本公司正式书面同意，其他任何个人、团体不得使用、 复制、修改或发布本软件.
+ *****************************************************************************/
+
+package com.noteshare.designPatterns.singleton;
+
+
+/**
+* @Title:
+* @author  陈海新
+* @since   JDK1.8
+* @history 2017年10月24日
+*/
+public class LazySingleton {
+
+}
