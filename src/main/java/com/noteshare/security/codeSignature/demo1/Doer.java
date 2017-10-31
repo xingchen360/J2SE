766707bda27c553d1adcccda2a86819d959556a2
陈海新 <chenhaixin@szboanda.net>
2017-10-31 19:34:18 +0800

    java keystore 权限控制

---
+++ src/main/java/com/noteshare/security/codeSignature/demo1/Doer.java
@@ -0,0 +1,12 @@
+package com.noteshare.security.codeSignature.demo1;
+
+
+/**
+* @Title:
+* @author  NoteShare
+* @since   JDK1.8
+* @history 2017年10月31日
+*/
+public interface Doer {
+    void doYourThing();
+}
---
+++ src/main/java/com/noteshare/security/codeSignature/demo1/Friend.java
@@ -0,0 +1,39 @@
+package com.noteshare.security.codeSignature.demo1;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+import org.apache.poi.ss.formula.functions.T;
+
+/**
+* @Title:访问控制的栈检查机制
+* @author  NoteShare
+* @since   JDK1.8
+* @history 2017年10月31日
+*/
+public class Friend implements Doer{
+    
+    private Doer next;
+    
+    private boolean direct;
+
+    public Friend(Doer next,boolean direct) {
+        this.next = next;
+        this.direct = direct;
+    }
+    
+    @Override
+    public void doYourThing() {
+        if(direct){
+            next.doYourThing();
+        }else{
+            AccessController.doPrivileged(new PrivilegedAction<T>() {
+                @Override
+                public T run() {
+                    next.doYourThing();
+                    return null;
+                }
+            });
+        }
+    }
+}
