d08f58c41b862a47a0e216ed07a0d1074bf727e8
陈海新 <chenhaixin@szboanda.net>
2017-11-03 18:18:50 +0800

    枚举测试

--- src/main/java/com/noteshare/enums/GenderEnum.java
+++ src/main/java/com/noteshare/enums/GenderEnum.java
@@ -3,7 +3,7 @@ package com.noteshare.enums;
 /**
  * 实现带有构造器的枚举
  */
-public enum GenderEnum {
+public enum GenderEnum{
 	//通过括号赋值,而且必须带有一个参构造器和一个属性跟方法，否则编译出错
     //赋值必须都赋值或都不赋值，不能一部分赋值一部分不赋值；如果不赋值则不能写构造器，赋值编译也出错
     MAN("0","男"), WOMEN("1","女");
