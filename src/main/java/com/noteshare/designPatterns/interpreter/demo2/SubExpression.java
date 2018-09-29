96adf3bec99b9621fa69eb64ed7c4d5238120ae9
NoteShare <chenhaixin@szboanda.net>
2018-09-29 14:12:54 +0800

    修改解释器模式代码注释

--- src/main/java/com/noteshare/designPatterns/interpreter/demo2/SubExpression.java
+++ src/main/java/com/noteshare/designPatterns/interpreter/demo2/SubExpression.java
@@ -1,5 +1,8 @@
 package com.noteshare.designPatterns.interpreter.demo2;
 
+/**
+ * 非终结符表达式
+ */
 public class SubExpression  extends OperatorExpression{
 
 	public SubExpression(Expression left, Expression right) {
